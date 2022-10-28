ffc5b8f5641783b88368412d867f263aa9b2f0d6
Nazar <nazar20050716@gmail.com>
2022-10-28 17:51:53 +0200

    move files to lesson 4 and start work on ternary operator

---
+++ src/com/example/examcloud/university/Task1_3.java
@@ -0,0 +1,23 @@
+package com.example.examcloud.university;
+
+public class Task1_3 {
+    public static void main(String[] args) {
+        //Task1
+        for (int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+        //Task2
+        for( int pow=1;pow<=1024;pow=pow*2){
+            System.out.println(pow);
+        }
+        //Task3
+        int a=10;
+        while (a>11){
+            System.out.println("while");
+        }
+        do{
+            System.out.println("do-while");
+        }while(a>11);
+    }
+}
+
---
+++ src/com/example/examcloud/university/Task5.java
@@ -0,0 +1,14 @@
+package com.example.examcloud.university;
+
+public class Task5 {
+    public static void main(String[] args) {
+        for (int rows=0;rows<7;rows++){
+            for (int col=0;col<7;col++){
+                if((rows+col)%2==0)
+                    System.out.print("o");
+                else System.out.print("x");
+            }
+            System.out.println();
+        }
+    }
+}
