044d446a21752ee0fb732d51788febc8d56a6794
Nazar <nazar20050716@gmail.com>
2022-11-25 19:42:27 +0100

    25.11 Task03 homework

---
+++ src/com/example/university/ppj11/Task03HomeArrays.java
@@ -0,0 +1,29 @@
+package com.example.university.ppj11;
+
+public class Task03HomeArrays {
+    public static void main(String[] args) {
+        int[] array = {2, 6, 4};
+        int max = array[0];
+        for (int col = 0; col < array.length; col++) {
+            if (max < array[col]) {
+                max = array[col];
+            }
+        }
+        for (int row = 6; row > 0; row--) {
+           for (int col = 0; col < array.length; col++) {
+                if ( 7 - row <= array[col]) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+// * * *
+// * * *
+//   * *
+//   * *
+//   *
+//   *
--- src/com/example/university/ppj11/Task03Teacher.java
+++ src/com/example/university/ppj11/Task03Teacher.java
@@ -28,17 +28,25 @@ public class Task03Teacher {
 //           }while (col <= row);
 //           System.out.println();
 //       }
+//        for (int row = 0; row < 4; row ++) {
+//            int size = 0;
+//            do {
+//                size ++;
+//                System.out.print(" * ");
+//            }while (size < 4 - row);
+//            System.out.println();
+//            row = 0, size = 4
+//           row = 1, size = 3
+//            row = 2, size = 2
+//            size = 4 - row
+//        }
         for (int row = 0; row < 4; row ++) {
             int size = 0;
             do {
                 size ++;
                 System.out.print(" * ");
-            }while (size < 4 - row);
+            } while ( 4 - size >  row);
             System.out.println();
-//            row = 0, size = 4
-//            row = 1, size = 3
-//            row = 2, size = 2
-//            size = 4 - row
         }
     }
 }
@@ -46,4 +54,3 @@ public class Task03Teacher {
 //  * * *
 //    * *
 //      *
-//
